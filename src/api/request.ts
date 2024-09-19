833aefa9dc210cac29eed15beaf8f4f7675e05b1
Yaroslav Vavilkin <y.vavilkin@tg.dunice.net>
2024-09-19 19:58:35 +0300

    refactor: improve code

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,10 +1,9 @@
-import { IOptionWithLabel, IOptionWithName } from '../interfaces';
 import { delay } from '../utils';
 
 import { MOCK_FIST_OPTIONS, MOCK_SECOND_OPTIONS } from './mock';
 import { TOptionID } from './mock/firstOptions';
 
-export const request = async <T extends IOptionWithName | IOptionWithLabel>(
+export const request = async <T>(
     path: string,
     data?: TOptionID
 ): Promise<T[] | never> => {
